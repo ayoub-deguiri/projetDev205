d57d0dec2b1d9b723983fc0d8be77f497b7b8c19
Holako <holakonoob@gmail.com>
2022-10-22 14:15:16 +0100

    Changes to be committed: 	modified:   PageDirectrice-Afich.html 	modified:   accueil-directrice.html 	modified:   back-end/db.php 	modified:   responsable.html
    modified links for Deconexion btn + fix bugs on db connection

--- back-end/db.php
+++ back-end/db.php
@@ -1,15 +1,14 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "root";
+$password = "";
 $db = "WFS205";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
-} catch(PDOException $e) {
+} catch (PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
-?>
