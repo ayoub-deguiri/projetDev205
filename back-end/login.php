df1a3b4864c043f4bd7d780c02b9dd4a5cbe03fb
Holako <holakonoob@gmail.com>
2022-10-22 14:01:38 +0100

    Changes to be committed: 	modified:   back-end/login.php 	modified:   login.html 	modified:   scripts/login.js

--- back-end/login.php
+++ back-end/login.php
@@ -1,3 +1,45 @@
-<?php 
-$matricule = $_POST['matricule'];
-$password = $_POST['password'];
+<?php
+include('db.php');
+session_start();
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // get user and password
+    $user = $_POST["matricule"];
+    $password = $_POST["password"];
+
+    // Validate Form Data  
+    $user = htmlspecialchars($user);
+    $user = trim($user);
+    $user = stripslashes($user);
+
+    $password = htmlspecialchars($password);
+    $password = trim($password);
+    $password = stripslashes($password);
+
+    // feching the data 
+    $sql = "SELECT * FROM  compte  WHERE user = ? and password = ?";
+    $pdo_statement = $conn->prepare($sql);
+    $pdo_statement->bindParam(1, $user);
+    $pdo_statement->bindParam(2, $password);
+    $pdo_statement->execute();
+    $result = $pdo_statement->fetch();
+
+
+    // redrection to main pages 
+    if (empty($result)) {
+        header('location:./../login.html');
+    } else {
+        if ($result['compteType'] == 'stagire') {
+            header('location:./../responsable.html');
+        } elseif ($result['compteType'] == 'directrice') {
+            header('location:./../accueil-directrice.html');
+        }
+        // anzidoha fach yt9ado les page t surveillance general
+        //elseif ($result['compteType'] == 'sg') {
+        //     header('location:./../accueil-sg.html');
+        // }
+    }
+}
+
+// created by Rguibi Marouane  and Idrissi Mohammed
--- scripts/login.js
+++ scripts/login.js
@@ -1,57 +1,52 @@
-const form=document.getElementById('form');
-const matricule=document.getElementById('matricule');
-const password=document.getElementById('password');
+const form = document.getElementById("form");
+const matricule = document.getElementById("matricule");
+const password = document.getElementById("password");
 
-
-
-form.addEventListener('submit',e =>{
+form.addEventListener("submit", (e) => {
+  if (!checkInputs()) {
     e.preventDefault();
+  }
+});
 
-    checkInputs();
-})
+const setError = (element, message) => {
+  const formControl = element.parentElement;
+  const errorDisplay = formControl.querySelector(".error");
 
-const setError=(element,message)=>{
-    const formControl=element.parentElement;
-    const errorDisplay=formControl.querySelector('.error');
+  errorDisplay.innerText = message;
+  formControl.classList.add("error");
+  formControl.classList.remove("success");
+};
 
-    errorDisplay.innerText=message;
-    formControl.classList.add('error');
-    formControl.classList.remove('success');
-}
+const setSuccess = (element) => {
+  const formControl = element.parentElement;
+  const errorDisplay = formControl.querySelector(".error");
 
-const setSuccess=element=>{
-    const formControl=element.parentElement;
-    const errorDisplay=formControl.querySelector('.error');
+  errorDisplay.innerText = "";
+  formControl.classList.add("success");
+  formControl.classList.remove("error");
+};
 
-    errorDisplay.innerText='';
-    formControl.classList.add('success');
-    formControl.classList.remove('error');
+const checkInputs = () => {
+  const matriculeValue = matricule.value.trim();
+  const passwordValue = password.value.trim();
+  let etat1 = false;
+  let etat2 = false;
+  if (matriculeValue === "") {
+    setError(matricule, "matricule est obligatoire");
+  } else {
+    setSuccess(matricule);
+    etat1 = true;
+  }
 
-}
-
-
-
-const checkInputs=()=>{
-    const matriculeValue=matricule.value.trim();
-    const passwordValue=password.value.trim(); 
-
-    if(matriculeValue===''){
-        setError(matricule,'matricule est obligatoire')
-    } 
-    else{
-        setSuccess(matricule)
-     
-    }
-
-
-    if(passwordValue===''){
-        setError(password,'mot de passe est obligatoire')
-    }
-    else{
-        setSuccess(password)
-    }
-}
-
-
-
-
+  if (passwordValue === "") {
+    setError(password, "mot de passe est obligatoire");
+  } else {
+    setSuccess(password);
+    etat2 = true;
+  }
+  if (etat1 === true && etat2 === true) {
+    return true;
+  } else {
+    return false;
+  }
+};
