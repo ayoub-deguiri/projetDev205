eb4051892ef3ebc7961447b013eb314afac9d195
Anasjrr <anaskacm89@gmail.com>
2022-12-01 20:52:39 +0100

    m

--- Modifier-Stagiaire.php
+++ Modifier-Stagiaire.php
@@ -175,27 +175,18 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["AjaxValider"])) {
     </header>
     <nav>
         <ul>
-            <li>
-                <a href="./Accueil-serveillant.php"><button><i class="fa fa-home"
-                            aria-hidden="true"></i>ACCUEIL</button></a>
-            </li>
-            <li>
-                <a href="./Modifier-Stagiaire.php"><button><i class="fa fa-pencil-square"
-                            aria-hidden="true"></i>MODIFIER</button></a>
-            </li>
-            <li>
-                <a href=""><button><i class="fa fa-calendar-times-o" aria-hidden="true"></i>ABSENCE</button></a>
-                <ul>
-                    <li><a href="./Affichage-surveillant.php"><button>Affichage</button></a></li>
-                    <li><a href="./SasireAbsence-surveillant.php"><button>Saisir</button></a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="./note.html"><button><i class="fa fa-calendar" aria-hidden="true"></i>NOTES</button> </a>
-            </li>
-            <li>
-                <a href="#"><button><i class="fa fa-plus-circle" aria-hidden="true"></i>AJOUTER</button></a>
-            </li>
+        <li>
+            <a href="#"><button >Création</button></a>
+        </li>
+        <li>
+            <a href="#"><button class="module">Module</button></a> 
+        </li>
+        <li>
+            <a href="#"><button >Formateur</button></a>
+        </li>
+        <li>  
+            <a href="#"><button class="Stagiaire">Stagiaire</button> </a>
+        </li>
         </ul>
     </nav>
     <!-- Ajax select -->
