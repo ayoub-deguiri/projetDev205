24e0dc388a7f8942fc010eb315e41bbca3f42501
Anasjrr <anaskacm89@gmail.com>
2022-12-05 17:57:31 +0100

    modified:   Deperdition.php

--- Deperdition.php
+++ Deperdition.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" media="screen" href="./styles/DeperditionCss.css">
     <link rel="shortcut icon" href="./images/logoApp.png" type="image/x-icon">
-    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
   
     <title>Document</title>
 </head>
@@ -90,8 +90,8 @@
     <!--tableau-->
       <div>
        
-            <form> 
-            <main>
+            <main> 
+            <form>
                 
                 <table>
                     <tr>
@@ -249,22 +249,9 @@
 
                 </table>
                 
-           
+            </form>
 
             </main>
-            <div id="myModal" class="modal" role="dialog">
-
-                <!-- Modal content -->
-                <div class="modal-content">
-                    <div><span class="close">&times;</span></div>
-                    <div class="modal-header">
-                        <h4 class="modal-title">l'historique du stagiaire :</h4>
-                    </div>
-                    <div class="modal-body">
-                    </div>
-                </div>
-          </form>
-            </div>
       </div>
     <!--fin-tableau-->
     
@@ -272,51 +259,12 @@
     <!--footer-->
     <footer>
         <p>© Copyright | DevWFS205 |2022</p>
-    </footer> 
-    
-    <script>
-        // Get the modal
-        var modal = document.getElementById("myModal");
-
-      
-
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks on the button, open the modal
-        function modalfn() {
-                  modal.style.display = "block";
-                  
-                }
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-        modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-        } 
-    </script>      
+    </footer>   
 
 
     
-        <script>
-        function myFunction() {
- 
-            Swal.fire({
-            position: 'center',
-            icon: 'success',
-            title: 'message',
-            showConfirmButton: false,
-            timer: 3000
- })
-}
-    </script>
-   
+       
+  
     <!--fin-->          
 </body>
 </html>
