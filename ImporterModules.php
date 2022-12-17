92c5299f43311700f62615c51705ae44d1a9b34a
HolakoNoob <HolakoNoob@gmail.com>
2022-12-17 19:10:35 +0100

    function vers 1.0

--- ImporterModules.php
+++ ImporterModules.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,85 +11,96 @@
     <link rel="shortcut icon" href="./images/logoApp.png" type="image/x-icon">
     <title>modifier</title>
 </head>
+
 <body>
     <header>
         <div class="logoOfppt">
-            <img src="./images/Ofpptlogo.png"  alt="logoOfppt" id="logoOfppt">
+            <img src="./images/Ofpptlogo.png" alt="logoOfppt" id="logoOfppt">
         </div>
         <div class="logoApp">
-            <img src="./images/logoApp.png"  alt="logo" class="logoApp">
+            <img src="./images/logoApp.png" alt="logo" class="logoApp">
         </div>
         <div class="déconnexion">
-        <button type="button" id="Déconnexion"><a href="./logout.php">Déconnexion</a></button>
-    </div> 
+            <button type="button" id="Déconnexion"><a href="./logout.php">Déconnexion</a></button>
+        </div>
     </header>
     <nav>
         <ul>
-        <li>
-            <a href="./creation.php"><button >Création</button></a>
-        </li>
-        <li>
-            <a href="./ImporterModules.php"><button class="module">Module</button></a> 
-        </li>
-        <li>
-            <a href="./Main-Formateur.php"><button>Formateur</button></a>
-        </li>
-        <li>  
-            <a href="./Importer_stagiaire.php"><button>Stagiaire</button> </a>
-        </li>
+            <li>
+                <a href="./creation.php"><button>Création</button></a>
+            </li>
+            <li>
+                <a href="./ImporterModules.php"><button class="module">Module</button></a>
+            </li>
+            <li>
+                <a href="./Main-Formateur.php"><button>Formateur</button></a>
+            </li>
+            <li>
+                <a href="./Importer_stagiaire.php"><button>Stagiaire</button> </a>
+            </li>
         </ul>
     </nav>
-     <form action="" method="post">
-         <div class="container">
-              <ul>
-                  <li> <label for="année-scolaire">Année scolaire :</label>
-                    <select name="annee-Scolaire" id="année-scolaire" required><option value="" ></option><option value="1">test</option></select> </li>
-
-                  <li> <label for="année">Année :</label>
-                  <select id="année" name="annee" required><option value="" ></option><option value="1">test</option></select></li>
-                   
-                <li><input type="submit" onclick="checkobligatoire()" value="Valider" id="valider"> </li>
-              </ul>
-            </div>
-        </form>
-    <main>
-        <form action="" method="post">
-         <div class="selects">
+    <form action="" method="post">
+        <div class="container">
             <ul>
-                <li>  <label for=""><h3>Importer module</h3></label></li>
-                <li>  <div class="telecharger"> <img src="./images/upload-cloud.svg" alt="" srcset="" id="icon-upload">
-                    <input type="button" value="Télécharger" id="telecherger"></div></li>
-                
-                <li><div class="validerImporter">
-                    <input type="submit" value="Valider" id="valider" onclick=" return vk()"> <input type="file" name="" id="file"> 
-                    <label for="file" id='buttonPhoto' >Importer</label>
-                </div><span id="lbimport"></span></li>
-          
+                <li> <label for="année-scolaire">Année scolaire :</label>
+                    <select name="annee-Scolaire" id="année-scolaire" required>
+                        <option value=""></option>
+                        <option value="1">test</option>
+                    </select>
+                </li>
+
+                <li> <label for="année">Année :</label>
+                    <select id="année" name="annee" required>
+                        <option value=""></option>
+                        <option value="1">test</option>
+                    </select>
+                </li>
+
+                <li><input type="submit" onclick="checkobligatoire()" value="Valider" id="valider"> </li>
             </ul>
-        </form>
         </div>
-        
+    </form>
+    <main>
+        <form method="POST" action="./inc/ModuleUpload.php" enctype="multipart/form-data">
+            <div class="selects">
+                <ul>
+                    <li> <label for="">
+                            <h3>Importer module</h3>
+                        </label></li>
+                    <li>
+                        <div class="telecharger"> <img src="./images/upload-cloud.svg" alt="" srcset=""
+                                id="icon-upload">
+                            <input type="button" value="Télécharger" id="telecherger">
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="validerImporter">
+                            <input type="submit" name="Submit" value="Valider" id="valider" onclick=" return vk()">
+                            <input type="file" name="csv_file" id="file">
+                            <label for="file" id='buttonPhoto'>Importer</label>
+                        </div><span id="lbimport"></span>
+
+                    </li>
+
+                </ul>
+        </form>
+        <br>
+
+        </div>
+
         <div class="listeModules">
             <table>
                 <tr>
-                    
-                        <th>Filière</th>
-                        <th>Nom module</th>
-                    
-                </tr>
-                <tr>
-                    <td></td>
-                    <td>.</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td>.</td>
+
+                    <th>Filière</th>
+                    <th>Nom module</th>
 
                 </tr>
                 <tr>
                     <td></td>
                     <td>.</td>
-                    
                 </tr>
                 <tr>
                     <td></td>
@@ -108,18 +120,28 @@
                 <tr>
                     <td></td>
                     <td>.</td>
-                    
+
                 </tr>
                 <tr>
                     <td></td>
                     <td>.</td>
-                    
+
                 </tr>
-            
+                <tr>
+                    <td></td>
+                    <td>.</td>
+
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>.</td>
+
+                </tr>
+
             </table>
         </div>
         <div class="buttonInserer">
-           <a href="./Main-Formateur.php"><button type="button" id="inserer"> Insérer Formateur &#10154;</button></a> 
+            <a href="./Main-Formateur.php"><button type="button" id="inserer"> Insérer Formateur &#10154;</button></a>
         </div>
 
 
@@ -129,61 +151,56 @@
         <p>© Copyright | DevWFS205 |2022</p>
     </footer>
 
-<script>
-    var file = document.getElementById('file')
-    var lbimport = document.getElementById('lbimport')
-    function vk()
-    {
-        var etat;
-        if(file.value =='')
-        {
-            etat =false
-            lbimport.innerHTML='please check your import file '
+    <script>
+        var file = document.getElementById('file')
+        var lbimport = document.getElementById('lbimport')
+        function vk() {
+            var etat;
+            if (file.value == '') {
+                etat = false
+                lbimport.innerHTML = 'please check your import file '
+            }
+            else {
+                etat = true
+            }
+            return etat
         }
-        else{
-            etat =true
-        }
-        return etat 
-    }
-</script>
- <script>
-    var annéescolaire = document.getElementById('année-scolaire')
-    var année = document.getElementById('année')
-    function checkobligatoire()
-    {
-        var etat1,etat2;
-        if(annéescolaire.value =='')
-        {
-            etat1 =false
-            annéescolaire.style.border='2px solid red'
-        }
-        else{
-            etat1 =true
-            annéescolaire.style.border='2px solid var(--color2)'
-        }
-        if(année.value =='')
-        {
-            etat3 =false
-            année.style.border='2px solid red'
-        }
-        else{
-            etat3 =true
-            année.style.border='2px solid var(--color2)'
-        }
+    </script>
+    <script>
+        var annéescolaire = document.getElementById('année-scolaire')
+        var année = document.getElementById('année')
+        function checkobligatoire() {
+            var etat1, etat2;
+            if (annéescolaire.value == '') {
+                etat1 = false
+                annéescolaire.style.border = '2px solid red'
+            }
+            else {
+                etat1 = true
+                annéescolaire.style.border = '2px solid var(--color2)'
+            }
+            if (année.value == '') {
+                etat3 = false
+                année.style.border = '2px solid red'
+            }
+            else {
+                etat3 = true
+                année.style.border = '2px solid var(--color2)'
+            }
 
-        if(etat1 ==false|| etat2==false)
-        {
-     
-            return false
+            if (etat1 == false || etat2 == false) {
+
+                return false
+            }
+            else {
+
+                return true
+            }
+
+
         }
-        else{
-           
-            return true
-        }
-       
-       
-    }
-</script>
+    </script>
 
 </body>
+
 </html>
